8bd11e2cbda052127460725871af7902fa63043e
Dmand723 <dawson.simmons723@gmail.com>
2024-11-21 12:08:50 -0700

    Create script.js
    Added some scripting to show the current date and year in the footer of the  page

---
+++ script.js
@@ -0,0 +1,7 @@
+let currentDate = new Date().toLocaleDateString();
+let date = document.querySelector("#date");
+let currentYear = new Date().getFullYear();
+let year = document.querySelector("#year");
+
+year.textContent = currentYear;
+date.textContent = currentDate;
